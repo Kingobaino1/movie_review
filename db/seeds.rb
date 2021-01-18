791fd6f9e0cc1dac5227eabda1d0338f2c6dd776
Kingsley <kingobaino@gmail.com>
2021-01-18 19:38:31 +0100

    Add the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a main sample user.
+User.create!(full_name: 'Example User', user_name: 'euser',
+             email: 'user@example.com',
+             password: 'foobar',
+             password_confirmation: 'foobar')
+
+
+# Generate a bunch of additional users.
+5.times do |n|
+  full_name = Faker::Name.name
+  user_name = "user-#{n+1}"
+  email = "u-#{n+1}@example.com"
+  password = 'password'
+  User.create!(full_name: full_name, user_name: user_name, email: email, 
+               password: password,
+               password_confirmation: password)
+end
